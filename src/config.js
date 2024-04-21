bade72cf5a0327dea3b30a02a7792372abdac64b
jdstar1922 <famega12@gmail.com>
2024-04-20 18:50:57 -0600

    removed connection string

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //administracion de la configuracion
-export const MONGODB_URI = "mongodb+srv://famega12:Password1234@serviciosweb2.ianersj.mongodb.net/mern-tasks?retryWrites=true&w=majority&appName=ServiciosWeb2"
+export const MONGODB_URI = ""
 export const TOKEN_SECRET = "cualquiercosa";
